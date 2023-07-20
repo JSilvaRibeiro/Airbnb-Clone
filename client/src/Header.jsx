c9f188eb1626da9b8abf13e061433e76e6a5ccd6
JSilvaRibeiro <123755997+JSilvaRibeiro@users.noreply.github.com>
2023-07-20 18:05:18 -0400

    added register page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="flex justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to="/" className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -20,7 +20,7 @@ export default function Header() {
           />
         </svg>
         <span className="font-bold text-xl">airbnd</span>
-      </a>
+      </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
