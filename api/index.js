d04e8f3ccb204d904d2d12e4a20acf55056edda1
JSilvaRibeiro <123755997+JSilvaRibeiro@users.noreply.github.com>
2023-08-04 19:18:41 -0400

    Register & Login Validation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "aldskajf14jklh3244khkjhl";
 
 app.use(express.json());
 app.use(
@@ -21,17 +23,38 @@ mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
   res.json("Test okay");
 });
+
 //booking
 //OuMwvBlkiAE5CAC9
+
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const userDoc = await User.create({
-    name,
-    email,
-    password: bcrypt.hashSync(password, bcryptSalt),
-  });
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
 
-  res.json(userDoc);
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passwordValid = bcrypt.compareSync(password, userDoc.password);
+    if (passwordValid) {
+      jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret);
+      res.cookie("token").json("Password Validated");
+    } else {
+      res.status(422).json("Password Invalid");
+    }
+  } else {
+    res.json("not found");
+  }
 });
 
 app.listen(4000);
