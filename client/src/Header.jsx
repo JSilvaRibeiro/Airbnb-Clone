beae07bdb919feb9bcc16053143a753824913ab0
JSilvaRibeiro <123755997+JSilvaRibeiro@users.noreply.github.com>
2023-08-06 15:06:42 -0400

    updates

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
+  const { user } = useContext(UserContext);
   return (
     <header className="flex justify-between">
       <Link to="/" className="flex items-center gap-1">
@@ -76,6 +78,7 @@ export default function Header() {
             />
           </svg>
         </div>
+        {!!user && <div>{user.name}</div>}
       </Link>
     </header>
   );
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,9 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
+  useEffect(() => )
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
