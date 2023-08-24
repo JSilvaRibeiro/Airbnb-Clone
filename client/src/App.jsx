e94f5eb104624b829780e73dfaeff0f314d04fc5
JSilvaRibeiro <123755997+JSilvaRibeiro@users.noreply.github.com>
2023-08-24 17:32:25 -0400

    Added PlacePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { UserContextProvider } from "./UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import PlacesPage from "./pages/PlacesPage";
 import AddPlace from "./pages/AddPlace";
+import PlacePage from "./pages/PlacePage";
 
 axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
@@ -25,6 +26,7 @@ function App() {
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<AddPlace />} />
           <Route path="/account/places/:id" element={<AddPlace />} />
+          <Route path="/place/:id" element={<PlacePage />} />
         </Route>
       </Routes>
     </UserContextProvider>
---
+++ client/src/pages/PlacePage.jsx
@@ -0,0 +1,122 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function PlacePage() {
+  const { id } = useParams();
+  const [place, setPlace] = useState(null);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get("/places/" + id).then((response) => {
+      setPlace(response.data);
+    });
+  }, [id]);
+
+  if (!place) return "";
+
+  if (showAllPhotos) {
+    return (
+      <div className="absolute inset-0 bg-black text-white text-center min-h-screen">
+        <div className="justify-center bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl">All Photos</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed flex top-8 gap-1 py-2 px-4 rounded-2xl shadow shadow-gray-500 bg-white text-black"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div>
+                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+      <h1 className="text-2xl">{place.title}</h1>
+      <a
+        className="my-2 block font-semibold underline"
+        target="_blank"
+        href={"https://www.google.com/maps/place/" + place.address}
+      >
+        {place.address}
+      </a>
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+          <div>
+            {place.photos?.[0] && (
+              <div>
+                <img
+                  className="aspect-square object-cover"
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                />
+              </div>
+            )}
+          </div>
+          <div className="grid">
+            <div>
+              {place.photos?.[1] && (
+                <img
+                  className="aspect-square object-cover"
+                  src={"http://localhost:4000/uploads/" + place.photos[1]}
+                />
+              )}
+            </div>
+            <div className="overflow-hidden">
+              {place.photos?.[2] && (
+                <img
+                  className="aspect-square object-cover relative top-2"
+                  src={"http://localhost:4000/uploads/" + place.photos[2]}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => setShowAllPhotos(true)}
+          className="flex gap-1 absolute bottom-2 bg-white right-2 py-2 px-4 rounded-2xl shadow-md shadow-gray-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+            />
+          </svg>
+          Show more photos
+        </button>
+      </div>
+    </div>
+  );
+}
